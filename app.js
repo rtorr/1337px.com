b8fce58f53ba667de35eb2fe6ecd1194a07fd3a1
Richard <297045+rtorr@users.noreply.github.com>
2020-06-20 08:42:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.redirect('https://lynnandtonic.com/'));
+app.get("/", (req, res) => res.redirect(302, 'https://lynnandtonic.com/'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
